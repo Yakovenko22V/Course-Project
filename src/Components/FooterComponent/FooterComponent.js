1c2766de3f5ca31984ccf314e883033fc1bb1e9f
Yakovenko22V <freemailtag@gmail.com>
2022-01-18 11:30:14 +0200

    final amendments made

--- src/Components/FooterComponent/FooterComponent.js
+++ src/Components/FooterComponent/FooterComponent.js
@@ -4,7 +4,7 @@ import './footer.scss'
 
 const FooterComponent = () => {
     return (
-        <a className='aLink' href='https://github.com/Yakovenko22V/SkillUp.CourseProject/tree/main/my-app' target="_blank" rel="noopener noreferrer">
+        <a className='aLink' href='https://github.com/Yakovenko22V/Course-Project' target="_blank" rel="noopener noreferrer">
         <div className='footer' >
             <p>TMBD Movie App © 2022</p>
         </div>
